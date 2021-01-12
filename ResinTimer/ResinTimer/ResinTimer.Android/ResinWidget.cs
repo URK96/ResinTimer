2ece149dc24d6bf273f6d7db3962ba9310e687ed
최진혁 <chlwlsgur96@hotmail.com>
2021-01-12 20:03:11 +0900

    1.5.0 Beta 3
    - Remove widget config

--- ResinTimer/ResinTimer/ResinTimer.Android/ResinWidget.cs
+++ ResinTimer/ResinTimer/ResinTimer.Android/ResinWidget.cs
@@ -1,7 +1,14 @@
 ﻿using Android.App;
 using Android.Appwidget;
 using Android.Content;
+using Android.Graphics;
+using Android.Graphics.Drawables;
+using Android.Views;
 using Android.Widget;
+
+using AndroidX.Core.Content.Resources;
+
+using Xamarin.Essentials;
 
 namespace ResinTimer.Droid
 {
@@ -35,26 +42,10 @@ namespace ResinTimer.Droid
         {
             base.OnUpdate(context, appWidgetManager, appWidgetIds);
 
-            var remoteView = new RemoteViews(context.PackageName, Resource.Layout.ResinWidget);
-
             ResinEnvironment.LoadValues();
             ResinEnvironment.CalcResin();
 
-            var intent = new Intent(AppWidgetManager.ActionAppwidgetUpdate);
-            intent.PutExtra(AppWidgetManager.ExtraAppwidgetIds, appWidgetIds);
-            intent.PutExtra(KEY_CLICKUPDATE, VALUE_CLICKUPDATE);
-
-            var pIntent = PendingIntent.GetBroadcast(context, 0, intent, PendingIntentFlags.UpdateCurrent);
-
-            remoteView.SetTextViewText(Resource.Id.ResinWidgetCount, ResinEnvironment.resin.ToString());
-            remoteView.SetTextViewText(Resource.Id.ResinWidgetEndTime, ResinEnvironment.endTime.ToString());
-
-            remoteView.SetOnClickPendingIntent(Resource.Id.ResinWidgetRootLayout, pIntent);
-
-            foreach (int id in appWidgetIds)
-            {
-                appWidgetManager.UpdateAppWidget(id, remoteView);
-            }
+            UpdateLayout(context, appWidgetManager, appWidgetIds);
 
             if (isClick)
             {
@@ -62,6 +53,51 @@ namespace ResinTimer.Droid
 
                 isClick = false;
             }
+        }
+
+        private void UpdateLayout(Context context, AppWidgetManager manager, int[] appWidgetIds)
+        {
+            foreach (int id in appWidgetIds)
+            {
+                var remoteViews = (Preferences.Get($"{SettingConstants.WIDGET_BACKGROUND}_{id}", "White")) switch
+                {
+                    "Black" => new RemoteViews(context.PackageName, Resource.Layout.ResinWidget_Black),
+                    _ => new RemoteViews(context.PackageName, Resource.Layout.ResinWidget)
+                };
+
+                remoteViews.SetTextViewText(Resource.Id.ResinWidgetCount, ResinEnvironment.resin.ToString());
+                remoteViews.SetTextViewText(Resource.Id.ResinWidgetEndTime, ResinEnvironment.endTime.ToString());
+
+                remoteViews.SetOnClickPendingIntent(Resource.Id.ResinWidgetRootLayout, CreateClickIntent(context, appWidgetIds));
+
+                manager.UpdateAppWidget(id, remoteViews);
+            }
+        }
+
+        private PendingIntent CreateClickIntent(Context context, int[] appWidgetIds)
+        {
+            var intent = new Intent(AppWidgetManager.ActionAppwidgetUpdate);
+            intent.PutExtra(AppWidgetManager.ExtraAppwidgetIds, appWidgetIds);
+            intent.PutExtra(KEY_CLICKUPDATE, VALUE_CLICKUPDATE);
+
+            return PendingIntent.GetBroadcast(context, 0, intent, PendingIntentFlags.UpdateCurrent);
+        }
+
+        private GradientDrawable CreateBG(Context context, int id)
+        {
+            var colorString = Preferences.Get($"{SettingConstants.WIDGET_BACKGROUND}_{id}", "#FFFFFF");
+            var roundedDrawable = ResourcesCompat.GetDrawable(context.Resources, Resource.Drawable.rounded_background, null) as GradientDrawable;
+
+            roundedDrawable.SetColor(Color.ParseColor(colorString));
+
+            return roundedDrawable;
+        }
+
+        private Color CreateColor(int id)
+        {
+            var colorString = Preferences.Get($"{SettingConstants.WIDGET_BACKGROUND}_{id}", "#FFFFFF");
+
+            return Color.ParseColor(colorString);
         }
 
         public override void OnEnabled(Context context)

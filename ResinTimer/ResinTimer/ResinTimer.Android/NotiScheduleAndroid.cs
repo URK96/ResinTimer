5638a4ea4f5f0d1a93b461d2844d824b37252aac
최진혁 <chlwlsgur96@hotmail.com>
2022-03-09 22:46:11 +0900

    v1.16.2 Beta 2
    - Change android alarm type for notification

--- ResinTimer/ResinTimer/ResinTimer.Android/NotiScheduleAndroid.cs
+++ ResinTimer/ResinTimer/ResinTimer.Android/NotiScheduleAndroid.cs
@@ -16,12 +16,12 @@ namespace ResinTimer.Droid
 
         public NotiScheduleAndroid()
         {
-            manager = new NotiManager();
+            manager = new();
         }
 
         public override void Cancel<T>()
         {
-            NotifierAndroid notifier = new NotifierAndroid();
+            NotifierAndroid notifier = new();
 
             foreach (T item in manager.GetNotiList<T>())
             {
@@ -31,21 +31,21 @@ namespace ResinTimer.Droid
 
         public static void Cancel(Noti noti)
         {
-            NotifierAndroid notifier = new NotifierAndroid();
+            NotifierAndroid notifier = new();
 
             notifier.Cancel(noti.NotiId);
         }
 
         public override void Schedule<T>()
         {
-            NotifierAndroid notifier = new NotifierAndroid();
+            NotifierAndroid notifier = new();
             DateTime now = DateTime.Now;
 
             foreach (T item in manager.GetNotiList<T>())
             {
                 if (item.NotiTime > now)
                 {
-                    Notification notification = new Notification
+                    Notification notification = new()
                     {
                         Title = item.GetNotiTitle(),
                         Text = item.GetNotiText(),
@@ -61,12 +61,12 @@ namespace ResinTimer.Droid
 
         public static void Schedule<T>(Noti noti) where T : Noti
         {
-            NotifierAndroid notifier = new NotifierAndroid();
+            NotifierAndroid notifier = new();
             DateTime now = DateTime.Now;
 
             if (noti.NotiTime > now)
             {
-                Notification notification = new Notification
+                Notification notification = new()
                 {
                     Title = noti.GetNotiTitle(),
                     Text = noti.GetNotiText(),
@@ -81,7 +81,7 @@ namespace ResinTimer.Droid
 
         public override void ScheduleCustomNoti(string title, string message, int id, DateTime notiTime)
         {
-            NotifierAndroid notifier = new NotifierAndroid();
+            NotifierAndroid notifier = new();
 
             notifier.Notify(new Notification
             {
@@ -94,7 +94,7 @@ namespace ResinTimer.Droid
 
         public override void TestNoti(string message = "")
         {
-            NotifierAndroid notifier = new NotifierAndroid();
+            NotifierAndroid notifier = new();
 
             notifier.Notify(new Notification
             {
--- ResinTimer/ResinTimer/ResinTimer.Android/NotifierAndroid.cs
+++ ResinTimer/ResinTimer/ResinTimer.Android/NotifierAndroid.cs
@@ -4,20 +4,18 @@ using Android.OS;
 
 using AndroidX.Core.App;
 
-using Java.Util;
-
 using System;
 using System.IO;
 using System.Xml.Serialization;
-
-using ResinTimer.Models;
 
 namespace ResinTimer.Droid
 {
     public class NotifierAndroid
     {
-        public AlarmManager AlarmManager => Application.Context.GetSystemService(NotificationCompat.CategoryAlarm) as AlarmManager;
-        public NotificationManager NotiManager => Application.Context.GetSystemService(Context.NotificationService) as NotificationManager;
+        public AlarmManager AlarmManager => 
+            Application.Context.GetSystemService(NotificationCompat.CategoryAlarm) as AlarmManager;
+        public NotificationManager NotiManager => 
+            Application.Context.GetSystemService(Context.NotificationService) as NotificationManager;
 
         /// <summary>
         /// Notifies the specified notification.
@@ -27,7 +25,9 @@ namespace ResinTimer.Droid
         {
             long triggerTime = NotifyTimeInMilliseconds(notification.NotifyTime);
 
-            AlarmManager.Set(AlarmType.ElapsedRealtime, SystemClock.ElapsedRealtime() + triggerTime, CreatePendingIntent(notification.Id, notification));
+            AlarmManager.Set(AlarmType.ElapsedRealtimeWakeup, 
+                             SystemClock.ElapsedRealtime() + triggerTime, 
+                             CreatePendingIntent(notification.Id, notification));
         }
 
         /// <summary>
@@ -42,20 +42,22 @@ namespace ResinTimer.Droid
 
         private PendingIntent CreatePendingIntent(int id, Models.Notification noti = null)
         {
-            Intent intent = new Intent(Application.Context, typeof(ScheduledAlarmReceiver)).SetAction($"LocalNotiIntent{id}");
+            Intent intent = new Intent(Application.Context, typeof(ScheduledAlarmReceiver))
+                .SetAction($"LocalNotiIntent{id}");
 
-            if (noti != null)
+            if (noti is not null)
             {
                 intent.PutExtra(ScheduledAlarmReceiver.LocalNotificationKey, SerializeNotification(noti));
             }
 
-            return PendingIntent.GetBroadcast(Application.Context, 0, intent, PendingIntentFlags.UpdateCurrent | PendingIntentFlags.Mutable);
+            return PendingIntent.GetBroadcast(Application.Context, 0, intent,
+                                              PendingIntentFlags.UpdateCurrent | PendingIntentFlags.Mutable);
         }
 
         private string SerializeNotification(Models.Notification notification)
         {
-            XmlSerializer xmlSerializer = new XmlSerializer(notification.GetType());
-            using StringWriter stringWriter = new StringWriter();
+            XmlSerializer xmlSerializer = new(notification.GetType());
+            using StringWriter stringWriter = new();
 
             xmlSerializer.Serialize(stringWriter, notification);
 

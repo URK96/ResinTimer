a3308d03a2c9a2ed65d94cca901fb8487c7ab4b6
최진혁 <chlwlsgur96@hotmail.com>
2023-08-30 02:24:35 +0900

    [Add] Add notification permission related functions in NotiScheduleService declaration
    - Add CheckPlatformNotiEnabled function to check platform's notification permission is granted
    - Add RequestNotiPermission function to request platform's notification permission

--- ResinTimer/ResinTimer/ResinTimer/Services/INotiScheduleService.cs
+++ ResinTimer/ResinTimer/ResinTimer/Services/INotiScheduleService.cs
@@ -1,17 +1,28 @@
 ﻿using ResinTimer.Models.Notis;
 
 using System;
+using System.Threading.Tasks;
 
 namespace ResinTimer.Services
 {
     public interface INotiScheduleService
     {
         void Cancel<T>() where T : Noti;
+
         void Schedule<T>() where T : Noti;
+
         void ScheduleNotiItem<T>(Noti noti) where T : Noti;
+
         void ScheduleCustomNoti(string title, string message, int id, DateTime notiTime);
+
         void TestNoti(string message = "");
+
         void ScheduleAll();
+
         void CancelAll();
+
+        bool CheckPlatformNotiEnabled();
+
+        Task<bool> RequestNotiPermission();
     }
 }
--- ResinTimer/ResinTimer/ResinTimer/Services/NotiScheduleService.cs
+++ ResinTimer/ResinTimer/ResinTimer/Services/NotiScheduleService.cs
@@ -1,18 +1,32 @@
-﻿using ResinTimer.Models.Notis;
+﻿using ResinTimer.Dialogs;
+using ResinTimer.Models.Notis;
+using ResinTimer.Resources;
+using Rg.Plugins.Popup.Services;
 
 using System;
-using System.Collections.Generic;
-using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Essentials;
+
+using Xamarin.Forms;
 
 namespace ResinTimer.Services
 {
     public class NotiScheduleService : INotiScheduleService
     {
         public virtual void Cancel<T>() where T : Noti { }
+
         public virtual void Schedule<T>() where T : Noti { }
+
         public virtual void ScheduleNotiItem<T>(Noti noti) where T : Noti { }
+
         public virtual void ScheduleCustomNoti(string title, string message, int id, DateTime notiTime) { }
+
         public virtual void TestNoti(string message = "") { }
+
+        public virtual bool CheckPlatformNotiEnabled() => true;
+
+        public virtual async Task<bool> RequestNotiPermission() => await Task.FromResult(true);
 
         public void ScheduleAll()
         {
@@ -36,6 +50,39 @@ namespace ResinTimer.Services
             Cancel<GadgetNoti>();
             Cancel<FurnishingNoti>();
             Cancel<GardeningNoti>();
+        }
+
+        public static async Task VerifyNotificationAvailable()
+        {
+            if (!Preferences.Get(SettingConstants.NOTI_ENABLED, false))
+            {
+                return;
+            }
+
+            var notiService = DependencyService.Get<INotiScheduleService>();
+
+            if (!notiService.CheckPlatformNotiEnabled())
+            {
+                ActionView view = new(AppResources.VerifyNotiPermission_Dialog_Message);
+                BaseDialog dialog = new(AppResources.VerifyNotiPermission_Dialog_Title, view);
+
+                dialog.OnClose += async delegate
+                {
+                    if (view.Result)
+                    {
+                        if (!await notiService.RequestNotiPermission())
+                        {
+                            BaseDialog noticeDialog = new(
+                                string.Empty,
+                                new MessageView(AppResources.VerifyNotiPermission_AfterDenied_Message));
+
+                            await PopupNavigation.Instance.PushAsync(noticeDialog);
+                        }
+                    }
+                };
+
+                await PopupNavigation.Instance.PushAsync(dialog);
+            }
         }
     }
 }

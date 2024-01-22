0ee14c087c3c0860d24fdc5dd6005f6309cdd432
최진혁 <chlwlsgur96@hotmail.com>
2024-01-22 22:56:45 +0900

    [Add] Add v2 cookie input feature on manual cookie input page

--- ResinTimer/ResinTimer/ResinTimer/Pages/AccountSyncPages/AccountSettingPages/AccountCookieInputManualPage.xaml.cs
+++ ResinTimer/ResinTimer/ResinTimer/Pages/AccountSyncPages/AccountSettingPages/AccountCookieInputManualPage.xaml.cs
@@ -10,16 +10,46 @@ namespace ResinTimer.Pages.AccountSyncPages.AccountSettingPages
     [XamlCompilation(XamlCompilationOptions.Compile)]
     public partial class AccountCookieInputManualPage : ContentPage
     {
+        private enum CookieVersionType
+        {
+            V1 = 0,
+            V2 = 1
+        }
+
         public AccountCookieInputManualPage()
         {
             InitializeComponent();
+
+            foreach (string item in Enum.GetNames(typeof(CookieVersionType)))
+            {
+                CookieVersionPicker.Items.Add(item);
+            }
+
+            CookieVersionPicker.SelectedIndex = (int)CookieVersionType.V2;
         }
 
         private async void ApplyButtonClicked(object sender, EventArgs e)
         {
             try
             {
-                Utils.SetAccountCookieInfo(LtuidInputEntry.Text, LtokenInputEntry.Text);
+                string inputLtuid = LtuidInputEntry.Text;
+                string inputLtoken = LtokenInputEntry.Text;
+                var cookieVersion = (CookieVersionType)CookieVersionPicker.SelectedIndex;
+
+                switch (cookieVersion)
+                {
+                    case CookieVersionType.V2:
+                        {
+                            Utils.SetAccountV2CookieInfo(inputLtuid, inputLtoken);
+                        }
+                        break;
+
+                    default:
+                        {
+                            Utils.SetAccountCookieInfo(inputLtuid, inputLtoken);
+                        }
+                        break;
+                }
 
                 DependencyService.Get<IToast>().Show(AppResources.AccountSetting_Complete);
 
@@ -66,6 +96,27 @@ namespace ResinTimer.Pages.AccountSyncPages.AccountSettingPages
                 await button.ScaleTo(1.0, 100, Easing.SinInOut);
             }
             catch { }
+        }
+
+        private void CookieVersionPicker_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            if (sender is Picker)
+            {
+                var selectedCookieVersion = (CookieVersionType)CookieVersionPicker.SelectedIndex;
+
+                CookieLtuidTitleLabel.Text = selectedCookieVersion switch
+                {
+                    CookieVersionType.V2 => "ltuid v2",
+
+                    _ => "ltuid"
+                };
+                CookieLtokenTitleLabel.Text = selectedCookieVersion switch
+                {
+                    CookieVersionType.V2 => "ltoken v2",
+
+                    _ => "ltoken"
+                };
+            }
         }
     }
 }

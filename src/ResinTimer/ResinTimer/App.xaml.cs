43afc56198ab82d44277f71c6c0ca899551d278b
최진혁 <chlwlsgur96@hotmail.com>
2025-11-02 14:15:58 +0900

    [Add] Add new .NET MAUI solution & project to migrate existed app code

---
+++ src/ResinTimer/ResinTimer/App.xaml.cs
@@ -0,0 +1,15 @@
+﻿namespace ResinTimer
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+        }
+
+        protected override Window CreateWindow(IActivationState? activationState)
+        {
+            return new Window(new AppShell());
+        }
+    }
+}
---
+++ src/ResinTimer/ResinTimer/GlobalXmlns.cs
@@ -0,0 +1,2 @@
+[assembly: XmlnsDefinition("http://schemas.microsoft.com/dotnet/maui/global", "ResinTimer")]
+[assembly: XmlnsDefinition("http://schemas.microsoft.com/dotnet/maui/global", "ResinTimer.Pages")]
